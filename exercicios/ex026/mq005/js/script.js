30b3579ac9dfbeb0e762dff0a89338a26454f283
João Vitor Malta <126990089+joaovitormalta@users.noreply.github.com>
2024-08-29 13:45:22 -0300

    adaptando tamanhos

--- exercicios/ex026/mq005/js/script.js
+++ exercicios/ex026/mq005/js/script.js
@@ -6,3 +6,11 @@ function clicou() {
     }  
 
 }
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
